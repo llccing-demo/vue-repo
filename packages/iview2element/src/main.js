91acfae7f97cd61424d432f6178019b2e574b8d1
liuchunfeng <liuchunfeng@jd.com>
2019-09-10 16:07:05 +0800

    update 增加依赖

--- packages/iview2element/src/main.js
+++ packages/iview2element/src/main.js
@@ -2,6 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementUI from 'element-ui';
+import ('./element.variables.scss');
+// import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
+import './iview.variables.less';
+Vue.use(iView);
+
 Vue.config.productionTip = false
 
 new Vue({
