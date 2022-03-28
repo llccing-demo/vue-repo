afa41345de55fbae104fb8555897ee2ebe80f3d8
llccing <chunfeng.liu@innocellence.com>
2022-03-28 13:18:36 +0800

    init basic pwa

---
+++ packages/pwa/src/registerServiceWorker.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
+
+if (process.env.NODE_ENV === 'production') {
+  register(`${process.env.BASE_URL}service-worker.js`, {
+    // 4
+    ready () {
+      console.log(
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
+    },
+    // 1
+    registered () {
+      console.log('Service worker has been registered.')
+    },
+    // 3
+    cached () {
+      console.log('Content has been cached for offline use.')
+    },
+    // 2
+    updatefound () {
+      console.log('New content is downloading.')
+    },
+    updated () {
+      console.log('New content is available; please refresh.')
+    },
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
+    },
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
+}
